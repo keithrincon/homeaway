9f501b413fa7763a11616a2e73fe6cbb8f5e87b8
Keith Rincon <keithrt3008@gmail.com>
2024-09-23 11:49:06 -0700

    first commit

---
+++ app/favorites/loading.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import LoadingCards from '@/components/card/LoadingCards';
+import React from 'react';
+
+function loading() {
+  return <LoadingCards />;
+}
+
+export default loading;
---
+++ components/form/AmenitiesInput.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { use, useState } from 'react';
+import { conservativeAmenities, Amenity } from '@/utils/amenities';
+import { Checkbox } from '@/components/ui/checkbox';
+
+import React from 'react';
+
+function AmenitiesInput({ defaultValue }: { defaultValue?: Amenity[] }) {
+  const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(
+    defaultValue || conservativeAmenities
+  );
+
+  const handleChange = (conservativeAmenities: Amenity) => {
+    setSelectedAmenities((prev) => {
+      return prev.map((a) => {
+        if (a.name === conservativeAmenities.name) {
+          return { ...a, selected: !a.selected };
+        }
+        return a;
+      });
+    });
+  };
+
+  return (
+    <section>
+      <input
+        type='hidden'
+        name='amenities'
+        value={JSON.stringify(selectedAmenities)}
+      />
+      <div className='grid grid-cols-2 gap-4'>
+        {selectedAmenities.map((conservativeAmenities) => {
+          return (
+            <div
+              key={conservativeAmenities.name}
+              className='flex items-center space-x-2'
+            >
+              <Checkbox
+                id={conservativeAmenities.name}
+                checked={conservativeAmenities.selected}
+                onCheckedChange={() => handleChange(conservativeAmenities)}
+              />
+              <label
+                htmlFor={conservativeAmenities.name}
+                className='text-sm font-medium leading-none capitalize flex gap-x-2 items-center'
+              >
+                {conservativeAmenities.name}
+                <conservativeAmenities.icon className='w-4 h-4' />
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default AmenitiesInput;
