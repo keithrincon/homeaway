6f2174959c8cfbe664ba7135b414686bcea54603
Keith Rincon <keithrt3008@gmail.com>
2024-10-14 17:40:35 -0700

    errors

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,15 @@
 'use client';
+
 import { Input } from '../ui/input';
-import { useSearchParams, useRouter } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { useState, useEffect } from 'react';
 
 function NavSearch() {
   const searchParams = useSearchParams();
-  const { replace } = useRouter();
 
+  const pathname = usePathname();
+  const { replace } = useRouter();
   const [search, setSearch] = useState(
     searchParams.get('search')?.toString() || ''
   );
@@ -18,20 +20,19 @@ function NavSearch() {
     } else {
       params.delete('search');
     }
-    replace(`/?${params.toString()}`);
-  }, 500);
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   useEffect(() => {
     if (!searchParams.get('search')) {
       setSearch('');
     }
   }, [searchParams.get('search')]);
-
   return (
     <Input
-      type='text'
+      type='search'
       placeholder='find a property...'
-      className='max-w-xs dark:bg-muted'
+      className='max-w-xs dark:bg-muted '
       onChange={(e) => {
         setSearch(e.target.value);
         handleSearch(e.target.value);
